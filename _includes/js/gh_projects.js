0d30826364dadcf2bc6f6d5ad84f413665550d7d
Diva Dugar <diva.d@crowdanalytix.com>
2024-08-09 11:39:10 +0530

    trying to get the projects

--- _includes/js/gh_projects.js
+++ _includes/js/gh_projects.js
@@ -51,8 +51,8 @@ jQuery.fn.getRepos = function (username) {
             target.empty().append(list);
             $(repos).each(function () {
                 checkfork = this.fork;
-                // if ((this.name != (username.toLowerCase() + '.github.com')) && (checkfork != true)) { /* Check for username.github.com repo and for forked projects */
-                if (true) {
+                if ((this.name != (username.toLowerCase() + '.github.com')) && (checkfork != true)) { /* Check for username.github.com repo and for forked projects */
+                // if (true) {
                     list.append('<dt> \
                             <a style="font-size:20px;" href="' + (this.homepage ? this.homepage : this.html_url) + '"><h4 style="display: inline; padding-right: 2%;">' + this.name + '   </h4></a> \
                             <div style="display: inline-block;"><span class="lang" style="background:' + mapLangToColor(this.language) + '"></span> \
