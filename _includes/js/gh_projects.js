de998851ccf472d0e741cd5bb65214ef38676cdd
Diva D <divadugar@gmail.com>
2025-02-05 21:59:47 +0530

    changing gh_project.js

--- _includes/js/gh_projects.js
+++ _includes/js/gh_projects.js
@@ -27,16 +27,16 @@ jQuery.fn.getRepos = function (username) {
         function (data) {
             var repos = data.data; /* JSON Parsing */
             const filteredRepos = {};
-            filteredRepos["library-management-slack-bot"] = true;
-            filteredRepos["bazel-build_python_zip-bug-reproduction"] = true;
-            filteredRepos["dotfiles"] = true;
-            filteredRepos["homebrew-formulae"] = true;
-            filteredRepos["ghportfolio"] = true;
-            filteredRepos["thundergolfer.github.io"] = true;
-            filteredRepos["thundergolfer"] = true;
-            filteredRepos["golang-reactjs-skeleton-app"] = true;
-            filteredRepos["bazel-python-mypy-protobuf"] = true;
-            filteredRepos["arXie-Bot"] = true;
+            // filteredRepos["library-management-slack-bot"] = true;
+            // filteredRepos["bazel-build_python_zip-bug-reproduction"] = true;
+            // filteredRepos["dotfiles"] = true;
+            // filteredRepos["homebrew-formulae"] = true;
+            // filteredRepos["ghportfolio"] = true;
+            filteredRepos["divan009.github.io"] = true;
+            filteredRepos["divan009"] = true;
+            // filteredRepos["golang-reactjs-skeleton-app"] = true;
+            // filteredRepos["bazel-python-mypy-protobuf"] = true;
+            // filteredRepos["arXie-Bot"] = true;
 
             /* alert(repos.length); Only for checking how many items are returned. */
             try {
