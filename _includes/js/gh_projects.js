35db8dc605e20cd22d4397def13865341433780a
Diva D <divadugar@gmail.com>
2025-02-05 21:52:32 +0530

    checkin which file went wrong

--- _includes/js/gh_projects.js
+++ _includes/js/gh_projects.js
@@ -27,10 +27,17 @@ jQuery.fn.getRepos = function (username) {
         function (data) {
             var repos = data.data; /* JSON Parsing */
             const filteredRepos = {};
-            // filteredRepos["Cust_churn"] = true;
-            // filteredRepos["dumgo"] = true;
-            filteredRepos["divan009.github.io"] = true;
-            filteredRepos["divan009"] = true;
+            filteredRepos["library-management-slack-bot"] = true;
+            filteredRepos["bazel-build_python_zip-bug-reproduction"] = true;
+            filteredRepos["dotfiles"] = true;
+            filteredRepos["homebrew-formulae"] = true;
+            filteredRepos["ghportfolio"] = true;
+            filteredRepos["thundergolfer.github.io"] = true;
+            filteredRepos["thundergolfer"] = true;
+            filteredRepos["golang-reactjs-skeleton-app"] = true;
+            filteredRepos["bazel-python-mypy-protobuf"] = true;
+            filteredRepos["arXie-Bot"] = true;
+
             /* alert(repos.length); Only for checking how many items are returned. */
             try {
                 sortByForks(repos); /* Sorting by forks. You can customize it according to your needs. */
